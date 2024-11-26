454f7772d279a6af0fef70b1460d2f943b937acd
Mark Tsarkov <marktsarkov@MacBook-Pro-Mark.local>
2024-11-26 18:12:29 +0300

    remove grpc

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
-	desc "github.com/marktsarkov/sigma-service/internal/controller/grpc"
 	"github.com/marktsarkov/sigma-service/internal/controller/http"
 	repository "github.com/marktsarkov/sigma-service/internal/repo/note"
 	service "github.com/marktsarkov/sigma-service/internal/service/note"
-	pb "github.com/marktsarkov/sigma-service/pkg/note"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	"log"
-	"net"
 	"os"
 	"sync"
 
@@ -33,7 +29,8 @@ func Run(cfg *config.Environment, ctx context.Context) {
 	defer pool.Close()
 
 	//mongo
-	mg, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:admin@localhost:27017"))
+	URI := os.Getenv("MONGO_URI")
+	mg, err := mongo.Connect(ctx, options.Client().ApplyURI(URI))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -53,7 +50,7 @@ func Run(cfg *config.Environment, ctx context.Context) {
 	serv := service.NewNoteService(repo)
 
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	//http :8081
 	go func() {
 		defer wg.Done()
@@ -61,20 +58,6 @@ func Run(cfg *config.Environment, ctx context.Context) {
 		if err := app.Listen(fmt.Sprintf(":%v", (*cfg).GetPort())); err != nil {
 			log.Fatal(err)
 		}
-	}()
-
-	//grpc :8082
-	go func() {
-		defer wg.Done()
-		lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8082))
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-		var opts []grpc.ServerOption
-		grpcServer := grpc.NewServer(opts...)
-		reflection.Register(grpcServer)
-		pb.RegisterNoteServer(grpcServer, desc.NewNoteServer(pb.UnimplementedNoteServer{}, serv))
-		grpcServer.Serve(lis)
 	}()
 
 	wg.Wait()
